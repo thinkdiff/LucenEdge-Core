680fcffe613a31c9d8c284ec40d90a7d1d5558a1
thinkdiff <work.dhruvpathak@gmail.com>
2026-03-24 16:32:57 +0530

    Update canvas type casting to as HTMLCanvasElement for strict compilation

--- src/components/3d/HeroScene.tsx
+++ src/components/3d/HeroScene.tsx
@@ -5,7 +5,7 @@ export default function HeroScene() {
   const canvasRef = useRef<HTMLCanvasElement>(null)
 
   useEffect(() => {
-    const canvas = canvasRef.current!
+    const canvas = canvasRef.current as HTMLCanvasElement
     if (!canvas) return
     const ctx = canvas.getContext('2d')!
 
